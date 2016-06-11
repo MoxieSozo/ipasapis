ca150066c60628ee3fd72bc1b2b9974c0e927daa
Joey Vestal <joey.vestal@moxiesozo.com>
2016-06-11 01:31:03 -0600

    fonts

--- firebase/www/assets/js/controllers/farkle-controller.js
+++ firebase/www/assets/js/controllers/farkle-controller.js
@@ -1,3 +1,8 @@
 app.controller('farkleController', ['$scope', '$http', function( $scope, $http ){
 	
+	$('.rochambeer-controls i').on('click', function(){
+		$('.rochambeer-controls i').removeClass('active');
+		$(this).toggleClass('active');
+	});
+	
 }])
