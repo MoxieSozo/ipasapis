7fea1106f882b1510ddca0990c12139d310301b6
Moxie Sozo <accounts@moxiesozo.com>
2016-06-10 23:17:21 -0600

    SB: Added Beers + Series Controller
    Created views for series + beers + service for working with API

--- firebase/www/assets/js/app.js
+++ firebase/www/assets/js/app.js
@@ -31,6 +31,16 @@ app.config(function($stateProvider, $urlRouterProvider) {
       controller : 'farkleController',
       templateUrl: "templates/farkle.html"
     })
+    .state('beers', {
+      url: "/beers",
+      controller : 'beersController',
+      templateUrl: "templates/beers.html"
+    })
+    .state('seriesView', {
+      url: "/series/:series_id",
+      controller : 'seriesViewController',
+      templateUrl: "templates/series.view.html"
+    })
 });
 app.constant('APIURL', 'http://apis.mondorobot.com/')
 app.run(function(){
