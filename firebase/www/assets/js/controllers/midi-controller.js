7b80dcbd7140215f3a79b6b83cde002dcf4e6886
Moxie Sozo <accounts@moxiesozo.com>
2016-06-11 06:24:09 -0600

    SB

--- firebase/www/assets/js/controllers/midi-controller.js
+++ firebase/www/assets/js/controllers/midi-controller.js
@@ -126,17 +126,6 @@ app.controller('midiController', ['$scope', '$http', '$timeout','averyService', 
 }])
 
 
-function toColor(num) {
-    num >>>= 0;
-    var b = num & 0xFF,
-        g = (num & 0xFF00) >>> 8,
-        r = (num & 0xFF0000) >>> 16,
-        a = ( (num & 0xFF000000) >>> 24 ) / 255 ;
-		alert(r,g,b , a)
-    return "rgba(" + [r, g, b, a].join(",") + ")";
-}
-toColor(1)
-
 
 
 app.filter('floatToColor', [function(){
