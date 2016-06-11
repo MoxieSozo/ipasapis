5ef6ca68e758c11758beb1427d0653b12a414df1
Moxie Sozo <accounts@moxiesozo.com>
2016-06-11 02:55:55 -0600

    Added midi to navigation

--- firebase/www/assets/js/controllers/games-controller.js
+++ firebase/www/assets/js/controllers/games-controller.js
@@ -1,7 +1,8 @@
 app.controller('gamesController', ['$scope', '$state', function( $scope, $state ){
 	$scope.games = [
-		{'title' : 'Rochambeer', 'id' : 'farkle'},
-		{'title' : 'Brew a Beer', 'id' : 'tapper'}
+		{'title' : 'Rochambeer', 'id' : 'games.farkle'},
+		{'title' : 'Brew a Beer', 'id' : 'games.tapper'},
+		{'title' : 'Beer Midi', 'id' : 'midi'}
 	]	
 }])
 
